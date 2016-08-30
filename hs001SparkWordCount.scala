914973fca89e1d8106e3457c5f28ff659313d24d
lbingd17 <lbingd17@mail.ustc.edu.cn>
2016-08-30 11:04:52 +0800

    Update hs001SparkWordCount.scala

--- hs001SparkWordCount.scala
+++ hs001SparkWordCount.scala
@@ -10,9 +10,39 @@ object WordCount {
     }
     val conf = new SparkConf()
     val sc = new SparkContext(conf)
-    val line = sc.textFile(args(0))
-    line.flatMap(_.split(" ")).map((_, )).reduceByKey(_+_).
-    collect().foreach(println)
+    
+    //word count example
+    val textFile = sc.textFile(args(0))
+    textFile.flatMap(_.split(" ")).map((_, )).reduceByKey(_+_).collect().foreach(println) 
+    val counts = textFile.flatMap(line => line.split(“ “)).map(word => (word,1)).reduceByKey(+) 
+    val wordCounts = textFile.flatMap(line => line.split(" ")).map(word => (word, 1)).reduceByKey((a, b) => a + b) 
+
+    // Estimating Pi
+    val count = sc.parallelize(1 to 10000).map{
+      i => 
+      val x = Math.random() 
+      val y = Math.random() 
+      if (xx + yy < 1) 
+        1 
+      else 
+        0
+    }.reduce( + )
+    
+    //
+    var textFile = sc.textFile("/mnt/jediael/spark‐1.3.1‐bin‐hadoop2.6/README.md") 
+    textFile.count() 
+    textFile.first();
+    val linesWithSpark = textFile.filter(line => line.contains("Spark")) 
+    linesWithSpark.count() 
+    textFile.map(line => line.split(" ").size)
+    .reduce( (a, b) => if (a > b) a   else  b ) 
+  
+    import java.lang.Math 
+    textFile.map(line => line.split(" ").size).reduce((a, b) => Math.max(a, b))
+
+    linesWithSpark.cache() 
+
+
     sc.stop()
   }
 }
