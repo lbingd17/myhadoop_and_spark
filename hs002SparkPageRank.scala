3872e21b7a96471bcb03b3e5008f48da10be7b13
lbingd17 <lbingd17@mail.ustc.edu.cn>
2016-08-30 10:36:09 +0800

    Create hs002SparkPageRank.scala

---
+++ hs002SparkPageRank.scala
@@ -0,0 +1,18 @@
+//Example 4-25. Scala PageRank from Oreilly learning spark
+// Assume that our neighbor list was saved as a Spark objectFile
+  val links = sc.objectFile[(String, Seq[String])]("links")
+  .partitionBy(new HashPartitioner(100))
+  .persist()
+  // Initialize each page's rank to 1.0; since we use mapValues, the resulting RDD
+  // will have the same partitioner as links
+  var ranks = links.mapValues(v => 1.0)
+  // Run 10 iterations of PageRank
+  for (i <- 0 until 10) {
+  val contributions = links.join(ranks).flatMap {
+  case (pageId, (links, rank)) =>
+  links.map(dest => (dest, rank / links.size))
+  }
+  ranks = contributions.reduceByKey((x, y) => x + y).mapValues(v => 0.15 + 0.85*v)
+  }
+  // Write out the final ranks
+  ranks.saveAsTextFile("ranks")
